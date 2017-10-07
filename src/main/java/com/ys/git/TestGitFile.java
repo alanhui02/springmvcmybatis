8a022aa6a1d4eeded2e0b82c21bb2e58ce9cfdea
yaojihui <978369868@qq.com>
2017-10-07 21:32:23 +0800

    s
    s

--- src/main/java/com/ys/git/TestGitFile.java
+++ src/main/java/com/ys/git/TestGitFile.java
@@ -4,7 +4,7 @@ public class TestGitFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("时为准");
+		System.out.println("时为准"); System.out.println("时为准");
 		System.getProperties();
 		System.getProperties();
 		System.out.println(System.getProperties());
